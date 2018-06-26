95e8c19213856a4e48b29bc6a4c7a9678882e48d
conbag <cogrady91@gmail.com>
2018-06-26 11:15:51 +0100

    Add 4 tiles to Home Screen and remove quote

--- Components/HomeScreenTile.js
+++ Components/HomeScreenTile.js
@@ -1,62 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-import Icon from "react-native-vector-icons/Ionicons";
-import {themeStyles, Tiles} from "../Styles/TabStyles";
-import {deviceHeight, smallDeviceHeight} from "../Constants/Constants";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // Component used for each of the SP items on the first SP tab screen
 
-export const HomeScreenTile = props => {
-    return (
-        <View style={tileStyle.container}>
-            <TouchableHighlight
-                underlayColor="#FDEDEC"
-                style={[tileStyle.button, themeStyles.tiles]}
-                onPress={props.onPress}>
-                <View style={{alignItems:"center", justifyContent: 'space-evenly', flex: 1}}>
-                    <Text style={[tileStyle.buttonText, themeStyles.tileFont]}>{props.name}</Text>
-                    <Icon
-                        name={props.iconName}
-                        size={40}
-                        color={Tiles.iconColor}
-                    />
-                </View>
-            </TouchableHighlight>
+export const HomeScreenTile = (props) => {
+  return (
+    <View style={tileStyle.container}>
+      <TouchableHighlight underlayColor="#FDEDEC" style={tileStyle.button}>
+        <View style={{ alignItems: 'center' }}>
+          <Text style={tileStyle.buttonText}>{props.name}</Text>
+          <Icon name={props.iconName} size={90} />
         </View>
-    )
+      </TouchableHighlight>
+    </View>
+  );
 };
 
 HomeScreenTile.propTypes = {
-    name: PropTypes.string.isRequired,
-    iconName: PropTypes.string.isRequired,
-    onPress: PropTypes.func
+  name: PropTypes.string.isRequired,
+  iconName: PropTypes.string.isRequired,
 };
 // Defining prop types for this component
 
 const tileStyle = StyleSheet.create({
-    button: {
-        flex: 1,
-        //justifyContent: "space-around",
-        alignItems: "center",
-        //borderWidth: 1,
-        borderRadius: 7,
-        //backgroundColor: 'white'
-    },
+  button: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderWidth: 1,
+  },
 
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        marginHorizontal: 15,
-        //marginVertical: 23,
-        // margin: 10
-    },
-    // need to wrap button in View in order to stretch to full width of screen using flexDirection
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  // need to wrap button in View in order to stretch to full width of screen using flexDirection
 
-    buttonText: {
-        fontSize: deviceHeight < smallDeviceHeight ? 12 : 14,
-        //paddingBottom: 20
-    },
-
+  buttonText: {
+    fontSize: 25,
+    paddingBottom: 10,
+  },
 });
--- Components/HomeScreenTileRow.js
+++ Components/HomeScreenTileRow.js
@@ -1,44 +1,28 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-import { HomeScreenTile } from "./HomeScreenTile";
+import { HomeScreenTile } from './HomeScreenTile';
 
-export const HomeScreenTileRow = props => {
-    return (
-        <View style={tileRowStyle.container}>
-            <HomeScreenTile
-                name= {props.name1}
-                iconName= {props.iconName1}
-                onPress={props.onPress1}
-            />
-            <HomeScreenTile
-                name= {props.name2}
-                iconName= {props.iconName2}
-                onPress={props.onPress2}
-            />
-            {props.third && <HomeScreenTile
-                name= {props.name3}
-                iconName= {props.iconName3}
-                onPress={props.onPress3}
-            />}
-        </View>
-    )
+export const HomeScreenTileRow = (props) => {
+  return (
+    <View style={tileRowStyle.container}>
+      <HomeScreenTile name={props.name1} iconName={props.iconName1} />
+      <HomeScreenTile name={props.name2} iconName={props.iconName2} />
+    </View>
+  );
 };
 
 HomeScreenTileRow.propTypes = {
-    name1: PropTypes.string.isRequired,
-    name2: PropTypes.string.isRequired,
-    iconName1: PropTypes.string.isRequired,
-    iconName2: PropTypes.string.isRequired,
-    onPress1: PropTypes.func,
-    onPress2: PropTypes.func
+  name1: PropTypes.string.isRequired,
+  name2: PropTypes.string.isRequired,
+  iconName1: PropTypes.string.isRequired,
+  iconName2: PropTypes.string.isRequired,
 };
 
 const tileRowStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        marginTop: 35
-    }
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
 });
