ef0833c9f7ac992354aa87189fd700ebfc0dd1ca
conbag <cogrady91@gmail.com>
2018-07-04 17:07:52 +0100

    adding form to Coping Strategy screen along with DB helper methods

---
+++ Constants/DatabaseHelper.js
@@ -0,0 +1,28 @@
+// File containing useful function for interacting with SQLite database
+
+import React from 'react';
+import { db } from './DatabaseConnector';
+
+const numberParameters = (number) => {
+  if (number !== 1) {
+    return '?,'.repeat(number - 1) + '?';
+  } else {
+    return '?';
+  }
+};
+// Function that produces String of '?' based on number of columns to write to
+
+export const updateDatabase = (tableName, writeData, columns) => {
+  db.transaction(
+    (tx) => {
+      tx.executeSql(
+        `insert into ${tableName} (${columns.toString()}) values (${numberParameters(columns.length)})`,
+        writeData
+      );
+    },
+    (err) => console.log(err),
+    () => console.log('DB update success')
+  );
+};
+// Function that takes table name (String), data to write (array) and column names
+// to write to (array) as arguments. See 'CopingStrategies.js' for example of usage
