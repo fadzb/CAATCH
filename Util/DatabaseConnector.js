bdf1795fbec5b751def197523f489ab6739a07e5
conbag <cogrady91@gmail.com>
2018-07-05 10:55:05 +0100

    ensuring that Coping Strategy form clears after save

--- Util/DatabaseConnector.js
+++ Util/DatabaseConnector.js
@@ -13,44 +13,42 @@ const file = directory + dbFileName;
 
 export const db = SQLite.openDatabase(dbFileName);
 
-export const checkDB = (callback) => {
-    FileSystem.getInfoAsync(directory)
+export const checkDB = () => {
+  FileSystem.getInfoAsync(directory)
     // firstly checking if directory exists
-        .then(fileObj => {
-            if (fileObj.exists) {
-                console.log("Directory exists")
-            } else {
-                console.log("Directory DOES NOT exist");
-                FileSystem.makeDirectoryAsync(directory)
-                    .catch(err => console.error(err));
-            }
+    .then((fileObj) => {
+      if (fileObj.exists) {
+        console.log('Directory exists');
+      } else {
+        console.log('Directory DOES NOT exist');
+        FileSystem.makeDirectoryAsync(directory).catch((err) => console.error(err));
+      }
+    })
+    .then(() =>
+      FileSystem.getInfoAsync(file)
+        // then check if file exists
+        .then((fileObj) => {
+          if (fileObj.exists) {
+            console.log('File exists');
+            //deleteDB();
+            // uncomment above to test deletion of .db file
+          } else {
+            console.log('File DOES NOT exist');
+            //download file here
+
+            FileSystem.downloadAsync(Asset.fromModule(require('../Database/caatch.db')).uri, file).catch((err) =>
+              console.log(err)
+            );
+            // Have to download file from project directory in order to copy to Expo file system
+          }
         })
-        .then(() => FileSystem.getInfoAsync(file)
-            // then check if file exists
-                .then(fileObj => {
-                    if (fileObj.exists) {
-                        console.log("File exists");
-                        callback()
-
-                        //deleteDB();
-                    } else {
-                        console.log("File DOES NOT exist");
-                        //download file here
-
-                        FileSystem.downloadAsync(Asset.fromModule(require('../Database/caatch.db')).uri, file)
-                            .then(res => console.log(res))
-                            .then(res => callback())
-                            .catch(err => console.log(err))
-                        // Have to download file from project directory in order to copy to Expo file system
-                    }
-                })
-                .catch(err => console.error(err))
-        )
-        .catch(err => console.error(err));
+        .catch((err) => console.error(err))
+    )
+    .catch((err) => console.error(err));
 };
 // function checks if .db file exists and copies pre-populated file if not. Does nothing if exists
 
 const deleteDB = () => {
-    FileSystem.deleteAsync(file)
+  FileSystem.deleteAsync(file);
 };
-// function to delete database from file system, FOR TESTING PURPOSES ONLY -> see line 35
+// function to delete database from file system, FOR TESTING PURPOSES ONLY -> see line 31
--- Util/DatabaseHelper.js
+++ Util/DatabaseHelper.js
@@ -1,56 +1,28 @@
 // File containing useful function for interacting with SQLite database
 
 import React from 'react';
-import { db } from './DatabaseConnector'
+import { db } from './DatabaseConnector';
 
 const numberParameters = (number) => {
-    if (number !== 1) {
-        return ("?,".repeat(number - 1) + "?");
-    } else {
-        return "?";
-    }
+  if (number !== 1) {
+    return '?,'.repeat(number - 1) + '?';
+  } else {
+    return '?';
+  }
 };
 // Function that produces String of '?' based on number of columns to write to
 
-export const updateDatabase = (tableName, writeData, columns, func, resultFunc) => {
-    db.transaction(tx => {
-        tx.executeSql(`insert into ${tableName} (${columns.toString()}) values (${numberParameters(columns.length)})`, writeData, resultFunc !== undefined && ((_, res) => resultFunc(res)), err => console.log(err))
-    }, err => console.log(err), func !== undefined &&(func))
+export const updateDatabase = (tableName, writeData, columns) => {
+  db.transaction(
+    (tx) => {
+      tx.executeSql(
+        `insert into ${tableName} (${columns.toString()}) values (${numberParameters(columns.length)})`,
+        writeData
+      );
+    },
+    (err) => console.log(err),
+    () => console.log('DB update success')
+  );
 };
 // Function that takes table name (String), data to write (array) and column names
-// to write to (array) as arguments. See 'NewCopingStrategy.js' for example of usage.
-// Important the object keys are named the same as DB columns/fields. INSERT function
-
-
-export const readDatabase = (column, table, resultFunc, func) => {
-    db.transaction(tx => {
-        tx.executeSql(`select ${column} from ${table}`, [], resultFunc !== undefined &&((_, resultSet) => (resultFunc(resultSet.rows._array))), (_, err) => console.log(err))
-    }, err => console.log(err), func !== undefined &&(func))
-
-};
-
-// Function for reading from DB. Takes callback function that takes
-// result array as only argument. Only works with * or one column for now
-
-
-export const updateDatabaseArgument = (tableName, writeData, columns, argument, func, resultFunc) => {
-    const columnValues = (columns.map(c => c + '=?')).toString();
-
-    db.transaction(tx => {
-        tx.executeSql(`update ${tableName} set ${columnValues}` + argument, writeData, resultFunc !== undefined && ((_, res) => resultFunc(res)), err => console.log(err))
-    }, err => console.log(err), func !== undefined &&(func))
-};
-// for UPDATING not INSERTING
-
-export const readDatabaseArg = (column, table, resultFunc, func, argument) => {
-    db.transaction(tx => {
-        tx.executeSql(`select ${column} from ${table} ` + argument, [], resultFunc !== undefined &&((_, resultSet) => (resultFunc(resultSet.rows._array))), (_, err) => console.log(err))
-    }, err => console.log(err), func !== undefined &&(func))
-};
-// function for reading DB with additional arguments like joins etc.
-
-export const deleteDatabaseRow = (table, argument, resultFunc) => {
-    db.transaction(tx => {
-        tx.executeSql(`delete from ${table} ` + argument, [], resultFunc !== undefined &&((_, resultSet) => (resultFunc(resultSet.rows._array))))
-    })
-};
+// to write to (array) as arguments. See 'CopingStrategies.js' for example of usage
