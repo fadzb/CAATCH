f902417b6dd6643f946dc13638141ebfa9faafe2
conbag <cogrady91@gmail.com>
2018-07-05 16:22:14 +0100

    adding icons to Safety Plan menu

--- Components/SafetyPlanSelector.js
+++ Components/SafetyPlanSelector.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // Component used for each of the SP items on the first SP tab screen
 
@@ -12,7 +13,14 @@ export const SafetyPlanSelector = (props) => {
         style={selectorStyle.button}
         onPress={() => props.press(props.screen)}
       >
-        <Text style={selectorStyle.buttonText}>{props.name}</Text>
+        <View style={selectorStyle.textIconContainer}>
+          <View style={selectorStyle.iconContainer}>
+            <Icon name={props.iconName} size={props.iconSize} />
+          </View>
+          <View style={selectorStyle.textContainer}>
+            <Text style={selectorStyle.buttonText}>{props.name}</Text>
+          </View>
+        </View>
       </TouchableHighlight>
     </View>
   );
@@ -22,6 +30,8 @@ SafetyPlanSelector.propTypes = {
   press: PropTypes.func.isRequired,
   screen: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  iconName: PropTypes.string.isRequired,
+  iconSize: PropTypes.number.isRequired,
 };
 // Defining prop types for this component
 
@@ -30,7 +40,6 @@ const selectorStyle = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
-    borderBottomWidth: 1,
   },
 
   container: {
@@ -41,5 +50,25 @@ const selectorStyle = StyleSheet.create({
 
   buttonText: {
     fontSize: 20,
+  },
+
+  textContainer: {
+    flex: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+  },
+
+  iconContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
+  textIconContainer: {
+    flex: 1,
+    flexDirection: 'row',
   },
 });
