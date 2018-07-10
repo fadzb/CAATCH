312d59fa06e0222ec2ad305310c20b5ba9fff7f0
conbag <cogrady91@gmail.com>
2018-07-10 17:21:57 +0100

    correcting date format and adding more pre-populated strategies

--- Components/HomeScreenTileRow.js
+++ Components/HomeScreenTileRow.js
@@ -7,8 +7,8 @@ import { HomeScreenTile } from './HomeScreenTile';
 export const HomeScreenTileRow = (props) => {
   return (
     <View style={tileRowStyle.container}>
-      <HomeScreenTile name={props.name1} iconName={props.iconName1} />
-      <HomeScreenTile name={props.name2} iconName={props.iconName2} />
+      <HomeScreenTile name={props.name1} iconName={props.iconName1} onPress={props.onPress1} />
+      <HomeScreenTile name={props.name2} iconName={props.iconName2} onPress={props.onPress2} />
     </View>
   );
 };
@@ -18,6 +18,8 @@ HomeScreenTileRow.propTypes = {
   name2: PropTypes.string.isRequired,
   iconName1: PropTypes.string.isRequired,
   iconName2: PropTypes.string.isRequired,
+  onPress1: PropTypes.func,
+  onPress2: PropTypes.func,
 };
 
 const tileRowStyle = StyleSheet.create({
--- Constants/Prepopulated.js
+++ Constants/Prepopulated.js
@@ -10,4 +10,49 @@ export const prePopCoping = [
   {
     copeName: 'Reading',
   },
+  {
+    copeName: 'Going to a movie',
+  },
+  {
+    copeName: 'Saving money',
+  },
+  {
+    copeName: 'Traveling',
+  },
+  {
+    copeName: 'Writing',
+  },
+  {
+    copeName: 'Talking with friends',
+  },
+  {
+    copeName: 'Having a quiet night',
+  },
+  {
+    copeName: 'Dancing',
+  },
+  {
+    copeName: 'Going to a beach',
+  },
+  {
+    copeName: 'Watching sports on TV',
+  },
+  {
+    copeName: 'Singing',
+  },
+  {
+    copeName: 'Going for a bike ride',
+  },
+  {
+    copeName: 'Baking',
+  },
+  {
+    copeName: 'Planning the future',
+  },
+  {
+    copeName: 'Going running',
+  },
+  {
+    copeName: 'Instant messaging someone',
+  },
 ];
