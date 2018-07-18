cb1c2fee24935141d6544c709bf5ea1c37237bee
conbag <cogrady91@gmail.com>
2018-07-18 16:59:00 +0100

    adding contact screens. Ability to import contacts from phone. building default thumbnails

---
+++ Screens/SafetyPlanScreens/ContactScreens/PhoneContacts.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput, SectionList } from 'react-native';
+import { ContactRow } from '../../../Components/ContactRow';
+
+import { TabStyles } from '../../../Styles/TabStyles';
+
+export default class PhoneContacts extends React.Component {
+  static navigationOptions = {
+    title: 'Phone Contacts',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      contacts: this.props.navigation
+        .getParam('contacts')
+        .map((c) => {
+          let phone = '';
+          let email = '';
+
+          if (c.phoneNumbers.length !== 0) {
+            phone = c.phoneNumbers[0].number;
+          }
+
+          if (c.emails.length !== 0) {
+            email = c.emails[0].email;
+          }
+
+          return {
+            name: c.name,
+            firstName: c.firstName,
+            surname: c.lastName,
+            phone: phone,
+            email: email,
+          };
+        })
+        .sort(this.compareNames),
+    });
+  }
+
+  compareNames = (contact1, contact2) => {
+    if (contact1.name.toUpperCase() < contact2.name.toUpperCase()) {
+      return -1;
+    } else if (contact1.name.toUpperCase() > contact2.name.toUpperCase()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+  renderItem = (obj) => (
+    <View style={phoneContactStyle.listContainer}>
+      <ContactRow
+        name={obj.item.name}
+        number={obj.item.phone}
+        onPress={() => this.props.navigation.navigate('newContact', { checkedContact: obj.item })}
+      />
+    </View>
+  );
+
+  renderSectionHeader = (obj) => (
+    <View style={phoneContactStyle.sectionHeader}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{obj.section.title}</Text>
+    </View>
+  );
+
+  render() {
+    const contactsByLetter = this.state.contacts.reduce((obj, contact) => {
+      const firstLetter = contact.name[0].toUpperCase();
+
+      return {
+        ...obj,
+        [firstLetter]: [...(obj[firstLetter] || []), contact],
+      };
+    }, {});
+
+    const sections = Object.keys(contactsByLetter)
+      .sort()
+      .map((letter) => ({
+        title: letter,
+        data: contactsByLetter[letter],
+      }));
+
+    return (
+      <View style={phoneContactStyle.viewContainer}>
+        <SectionList
+          renderItem={this.renderItem}
+          renderSectionHeader={this.renderSectionHeader}
+          sections={sections}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
+  }
+}
+
+const phoneContactStyle = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  listContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  sectionHeader: {
+    flex: 1,
+    alignItems: 'stretch',
+    padding: 15,
+  },
+});
