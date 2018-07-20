78fcb0150de739e683f5f90da8c70f79a7971aed
conbag <cogrady91@gmail.com>
2018-07-20 10:40:32 +0100

    adding Reason component to SP

--- Screens/SafetyPlanScreens/ReasonScreens/ReasonSummary.js
+++ Screens/SafetyPlanScreens/ReasonScreens/ReasonSummary.js
@@ -7,7 +7,7 @@ import Moment from 'moment';
 import { Video } from 'expo';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default class StrategySummary extends React.Component {
+export default class ReasonSummary extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.getParam('name'),
@@ -51,9 +51,9 @@ export default class StrategySummary extends React.Component {
 
     return (
       <View style={{ flex: 1 }}>
-        <Container style={stratSummaryStyle.viewContainer}>
+        <Container style={reasonSummaryStyle.viewContainer}>
           <Content>
-            <Card style={stratSummaryStyle.cardStyle}>
+            <Card style={reasonSummaryStyle.cardStyle}>
               <CardItem>
                 <Left>
                   <Body>
@@ -73,12 +73,12 @@ export default class StrategySummary extends React.Component {
                       />
                     </View>
                   )}
-                  <Text style={stratSummaryStyle.text}>{this.props.navigation.getParam('desc')}</Text>
+                  <Text style={reasonSummaryStyle.text}>{this.props.navigation.getParam('desc')}</Text>
                   {link !== null && (
-                    <View style={stratSummaryStyle.textLinkView}>
+                    <View style={reasonSummaryStyle.textLinkView}>
                       <Icon name="ios-link" size={20} style={{ paddingRight: 5 }} />
                       <Text
-                        style={stratSummaryStyle.urlText}
+                        style={reasonSummaryStyle.urlText}
                         onPress={() =>
                           this.props.navigation.push('planWeb', {
                             url: 'https://' + link,
@@ -109,7 +109,7 @@ export default class StrategySummary extends React.Component {
   }
 }
 
-const stratSummaryStyle = StyleSheet.create({
+const reasonSummaryStyle = StyleSheet.create({
   viewContainer: {
     flex: 1,
     backgroundColor: '#fff',
