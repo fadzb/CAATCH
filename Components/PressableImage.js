596d5ac5dc6bfce42b868380252725f6fb72e811
conbag <cogrady91@gmail.com>
2018-07-25 16:37:47 +0100

    creating image component to keep at fixed size within SP sections

--- Components/PressableImage.js
+++ Components/PressableImage.js
@@ -3,22 +3,17 @@
 import React from 'react';
 import { StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native';
 
-export const PressableImage = props => {
-    return (
-        <TouchableOpacity
-            onPress={() => props.onPressFunction()}>
-            <Image
-                source={props.source}
-                resizeMode={'cover'}
-                style={imageStyle.image}
-            />
-        </TouchableOpacity>
-    )
+export const PressableImage = (props) => {
+  return (
+    <TouchableOpacity onPress={() => props.onPressFunction()}>
+      <Image source={props.source} resizeMode={'cover'} style={imageStyle.image} />
+    </TouchableOpacity>
+  );
 };
 
 const imageStyle = StyleSheet.create({
-    image: {
-        width: (Dimensions.get('window').width) - 35,
-        height: (Dimensions.get('window').height) / 2.4
-    }
+  image: {
+    width: Dimensions.get('window').width - 35,
+    height: Dimensions.get('window').height / 2.4,
+  },
 });
