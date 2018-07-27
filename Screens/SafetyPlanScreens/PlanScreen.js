890e837007e8f860da101720ffaff9ff3f88fe10
conbag <cogrady91@gmail.com>
2018-07-27 11:24:21 +0100

    adding search boxes to all SP 'new' sections

--- Screens/SafetyPlanScreens/PlanScreen.js
+++ Screens/SafetyPlanScreens/PlanScreen.js
@@ -1,39 +1,72 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import {getPrePops} from "../../Constants/Prepopulated";
+import { getPrePops } from '../../Constants/Prepopulated';
 
-import {TabStyles} from "../../Styles/TabStyles";
-import {SafetyPlanSelector} from "../../Components/SafetyPlanSelector";
-import {Icons} from "../../Constants/Icon";
-import {SafetyPlanDbTables, SectionHeader} from "../../Constants/Constants";
-
-const iconSize = 46;
+import { TabStyles } from '../../Styles/TabStyles';
+import { SafetyPlanSelector } from '../../Components/SafetyPlanSelector';
+import { Icons } from '../../Constants/Icon';
 
 export default class PlanScreen extends React.Component {
-    static navigationOptions = {
-        title: SectionHeader.plan
-    };
+  static navigationOptions = {
+    title: 'Safety Plan',
+  };
 
-    componentDidMount() {
-        getPrePops();
-        // get all SP prepop items from DB
-    }
+  componentDidMount() {
+    getPrePops();
+    // get all SP prepop items from DB
+  }
 
-    handleItemPress = screen => {
-        this.props.navigation.push(screen);
-    };
-    // function that navigates to selected SP screen
+  handleItemPress = (screen) => {
+    this.props.navigation.push(screen);
+  };
+  // function that navigates to selected SP screen
 
-    render() {
-        return (
-            <View style={TabStyles.stackContainer}>
-                <SafetyPlanSelector iconName={Icons.warningSign + "-outline"} iconSize={iconSize} name={SectionHeader.signs} press={this.handleItemPress} screen="warning"/>
-                <SafetyPlanSelector iconName={Icons.copingStrategy + "-outline"} iconSize={iconSize} name={SectionHeader.strategies} press={this.handleItemPress} screen="coping"/>
-                <SafetyPlanSelector iconName={Icons.distractions + "-outline"} iconSize={iconSize} name={SafetyPlanDbTables.distraction.title} press={this.handleItemPress} screen="selectDistraction"/>
-                <SafetyPlanSelector iconName={Icons.lifeWorthLiving + "-outline"} iconSize={iconSize} name={SafetyPlanDbTables.reason.title} press={this.handleItemPress} screen="reason"/>
-                <SafetyPlanSelector iconName={Icons.helpers} iconSize={iconSize} name={SectionHeader.contacts} press={this.handleItemPress} screen="contact"/>
-                <SafetyPlanSelector iconName={Icons.environmentSafe + "-outline"} iconSize={iconSize} name={SafetyPlanDbTables.environment.title} press={this.handleItemPress} screen="environment"/>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={TabStyles.stackContainer}>
+        <SafetyPlanSelector
+          iconName={Icons.warningSign + '-outline'}
+          iconSize={50}
+          name="Warning Signs"
+          press={this.handleItemPress}
+          screen="warning"
+        />
+        <SafetyPlanSelector
+          iconName={Icons.copingStrategy + '-outline'}
+          iconSize={50}
+          name="Coping Strategies"
+          press={this.handleItemPress}
+          screen="coping"
+        />
+        <SafetyPlanSelector
+          iconName={Icons.lifeWorthLiving + '-outline'}
+          iconSize={50}
+          name="Life Worth Living"
+          press={this.handleItemPress}
+          screen="reason"
+        />
+        <SafetyPlanSelector
+          iconName={Icons.distractions + '-outline'}
+          iconSize={50}
+          name="Distractions"
+          press={this.handleItemPress}
+          screen="distraction"
+        />
+        <SafetyPlanSelector
+          iconName={Icons.contacts + '-outline'}
+          iconSize={50}
+          name="Contacts"
+          press={this.handleItemPress}
+          screen="contact"
+        />
+        <SafetyPlanSelector
+          iconName={Icons.environmentSafe + '-outline'}
+          iconSize={50}
+          name="Making the Environment Safe"
+          press={this.handleItemPress}
+          screen="environment"
+        />
+      </View>
+    );
+  }
 }
