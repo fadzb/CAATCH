a7717faccc3e52bc3f3c72338da7a1d63b5c212e
conbag <cogrady91@gmail.com>
2018-08-16 17:33:24 +0100

    adding archive function to general section in Diary

--- Screens/DiaryScreens/GeneralSession.js
+++ Screens/DiaryScreens/GeneralSession.js
@@ -1,149 +1,149 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
-import Icon from "react-native-vector-icons/Ionicons";
+import Icon from 'react-native-vector-icons/Ionicons';
 import Moment from 'moment';
-import store from "../../Redux/store"
-import {Icons} from "../../Constants/Icon";
-import {DiaryId} from "../../Constants/Constants";
-import {AppColors} from "../../Styles/TabStyles";
+import store from '../../Redux/store';
+import { Icons } from '../../Constants/Icon';
 
 export default class GeneralSession extends React.Component {
-    static navigationOptions = ({ navigation }) => {
-        const diaryDate = store.getState().diary.date;
+  static navigationOptions = ({ navigation }) => {
+    const diaryDate = store.getState().diary.date;
 
-        return {
-            title: Moment(diaryDate).format('LL'),
-        }
+    return {
+      title: Moment(diaryDate).format('LL'),
     };
+  };
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            general: this.props.navigation.getParam('resultsArr'),
-        }
-    }
-    // setting state as result array generated in GeneralSummary.js
+    this.state = {
+      general: this.props.navigation.getParam('resultsArr'),
+    };
+  }
+  // setting state as result array generated in GeneralSummary.js
 
-    renderItem = ({item}) => (
-        <View style={generalSessionStyle.listContainer}>
-            {item.diaryName !== 'Notes' ?
-            <GeneralRow
-                name= {item.diaryName}
-                rating={Math.round(item.rating) + (item.diaryId === DiaryId.steps ? "" : " / " + item.scale)}
-                circleView={item.diaryName.slice(0,1).toUpperCase()}
-                icon={Icons.dividerArrow}
-            /> :
-            <GeneralNote
-                name= {item.diaryName}
-                rating={item.rating}
-                circleView={item.diaryName.slice(0,1).toUpperCase()}
-                icon={Icons.dividerArrow}
-            />}
-        </View>
+  renderItem = ({ item }) => (
+    <View style={generalSessionStyle.listContainer}>
+      {item.diaryName !== 'Notes' ? (
+        <GeneralRow
+          name={item.diaryName}
+          rating={Math.round(item.rating) + ' / ' + item.scale}
+          circleView={item.diaryName.slice(0, 1).toUpperCase()}
+          icon={Icons.dividerArrow}
+        />
+      ) : (
+        <GeneralNote
+          name={item.diaryName}
+          rating={item.rating}
+          circleView={item.diaryName.slice(0, 1).toUpperCase()}
+          icon={Icons.dividerArrow}
+        />
+      )}
+    </View>
+  );
+
+  render() {
+    return (
+      <View style={generalSessionStyle.viewContainer}>
+        <FlatList
+          data={this.state.general}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
     );
-
-    render() {
-        return (
-            <View style={generalSessionStyle.viewContainer}>
-                <FlatList
-                    data={this.state.general}
-                    renderItem={this.renderItem}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-            </View>
-        );
-    }
+  }
 }
 
 const GeneralRow = (props) => (
-    <View style={generalSessionStyle.container}>
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: "flex-start", alignItems: 'center'}}>
-            <View style={generalSessionStyle.iconContainer}>
-                <Icon
-                    name={props.icon}
-                    size={30}
-                    color={AppColors.orange}/>
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text style={generalSessionStyle.buttonText}>{props.name}</Text>
-                <Text style={generalSessionStyle.ratingText}>{props.rating}</Text>
-            </View>
-        </View>
+  <View style={generalSessionStyle.container}>
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+      <View style={generalSessionStyle.iconContainer}>
+        <Icon name={props.icon} size={30} color={props.iconColor} />
+      </View>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={generalSessionStyle.buttonText}>{props.name}</Text>
+        <Text style={generalSessionStyle.ratingText}>{props.rating}</Text>
+      </View>
     </View>
+  </View>
 );
 // stateless component for rendering each saved general rating
 
-const GeneralNote = props => (
-    <View style={generalSessionStyle.noteContainer}>
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: "flex-start", alignItems: 'center', marginTop: 30, marginBottom: 30}}>
-            <View style={generalSessionStyle.iconContainer}>
-                <Icon
-                    name={props.icon}
-                    size={30}
-                    color={AppColors.orange}/>
-            </View>
-            <View style={{flex: 1}}>
-                <View style={{flex: 1, paddingRight: 10, justifyContent: 'center'}}>
-                    <Text style={generalSessionStyle.buttonText}>{props.rating}</Text>
-                </View>
-            </View>
+const GeneralNote = (props) => (
+  <View style={generalSessionStyle.noteContainer}>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 30,
+        marginBottom: 30,
+      }}
+    >
+      <View style={generalSessionStyle.iconContainer}>
+        <Icon name={props.icon} size={30} color={props.iconColor} />
+      </View>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, paddingRight: 10 }}>
+          <Text style={generalSessionStyle.buttonText}>{props.rating}</Text>
         </View>
+      </View>
     </View>
+  </View>
 );
 // stateless component for rendering each saved general note
 
 const generalSessionStyle = StyleSheet.create({
-    listContainer: {
-        alignSelf: 'stretch'
-    },
+  listContainer: {
+    alignSelf: 'stretch',
+  },
 
-    viewContainer: {
-        flex: 1,
-    },
+  viewContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 
-    container: {
-        flex: 1,
-        justifyContent: "space-around",
-        borderBottomWidth: 1,
-        marginLeft: 30,
-        marginRight: 30,
-        height: 80,
-        borderColor: AppColors.orange
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    borderBottomWidth: 1,
+    marginLeft: 30,
+    marginRight: 30,
+    height: 80,
+  },
 
-    noteContainer: {
-        flex: 1,
-        justifyContent: "space-around",
-        borderBottomWidth: 1,
-        marginLeft: 30,
-        marginRight: 30,
-        alignItems: 'center',
-        borderColor: AppColors.orange
-    },
+  noteContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    borderBottomWidth: 1,
+    marginLeft: 30,
+    marginRight: 30,
+    alignItems: 'center',
+  },
 
-    buttonText: {
-        fontSize: 18,
-        paddingLeft: 10,
-        color: AppColors.blue
-    },
+  buttonText: {
+    fontSize: 18,
+    paddingLeft: 10,
+  },
 
-    ratingText: {
-        fontSize: 18,
-        paddingRight: 10,
-        color: AppColors.blue
-    },
+  ratingText: {
+    fontSize: 18,
+    paddingRight: 10,
+  },
 
-    iconContainer: {
-        paddingLeft: 10,
-        width: 30,
-        height: 30,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  iconContainer: {
+    paddingLeft: 10,
+    width: 30,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    noteText: {
-        fontSize: 15,
-        paddingRight: 10
-    }
+  noteText: {
+    fontSize: 15,
+    paddingRight: 10,
+  },
 });
--- Screens/DiaryScreens/GeneralSummary.js
+++ Screens/DiaryScreens/GeneralSummary.js
@@ -1,135 +1,111 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
 import Moment from 'moment';
-import {SelectionRow} from "../../Components/SelectionRow";
-import {Icons} from "../../Constants/Icon";
-import store from "../../Redux/store"
-import {readDatabaseArg} from "../../Util/DatabaseHelper";
-import {DbTableNames} from "../../Constants/Constants";
-import {connect} from 'react-redux'
-import {updateDate} from "../../Redux/actions";
-import {DateChanger} from "../../Components/DateChanger";
+import { SelectionRow } from '../../Components/SelectionRow';
+import { Icons } from '../../Constants/Icon';
+import store from '../../Redux/store';
+import { readDatabaseArg } from '../../Util/DatabaseHelper';
 
 export default class GeneralSummary extends React.Component {
-    static navigationOptions = ({ navigation }) => {
-        return {
-            title: 'Archive',
-        }
+  static navigationOptions = ({ navigation }) => {
+    const diaryDate = store.getState().diary.date;
+
+    return {
+      title: 'Archive' + ' ' + Moment(diaryDate).format('DD.MM.YYYY'),
     };
+  };
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            general: [],
-            originalDate: store.getState().diary.date
-        }
-    }
-
-    componentWillUnmount() {
-        store.dispatch(updateDate(Moment(this.state.originalDate)));
-    }
-    // set global diary date to original dat on back press
-
-    componentDidMount() {
-        this.getArchive();
-    }
-    // query DB for previous general sessions on this date
-
-    backDay = () => {
-        const newDate = Moment(store.getState().diary.date).subtract(1, 'd').format('YYYY-MM-DD HH:mm:ss.SSS');
-
-        store.dispatch(updateDate(Moment(newDate)));
-        this.getArchive()
+    this.state = {
+      general: [],
     };
+  }
 
-    forwardDay = () => {
-        const newDate = Moment(store.getState().diary.date).add(1, 'd').format('YYYY-MM-DD HH:mm:ss.SSS');
+  componentDidMount() {
+    const diaryDate = store.getState().diary.date;
 
-        store.dispatch(updateDate(Moment(newDate)));
-        this.getArchive()
-    };
+    const selectedDate = Moment(diaryDate).format('YYYY-MM-DD');
+    const columns =
+      'd.sessionId, s.diaryDate, di.scale, s.dateEntered, d.diaryId, d.rating, di.diaryType, di.diaryName, di.info';
 
-    getArchive = () => {
-        const diaryDate = store.getState().diary.date;
-
-        const selectedDate = Moment(diaryDate).format("YYYY-MM-DD");
-        const columns = "d.sessionId, s.diaryDate, di.scale, s.dateEntered, d.diaryId, d.rating, di.diaryType, di.diaryName, di.info";
-
-        readDatabaseArg(columns,
-            DbTableNames.diarySession,
-            this.transformResults,
-            undefined,
-            " as d inner join " + DbTableNames.session + " as s on d.sessionId = s.sessionId inner join " + DbTableNames.diary + " as di on d.diaryId = di.diaryId" +
-            " where DATE(diaryDate) = '" + selectedDate + "' and diaryType = 'General'");
-    };
-
-    transformResults = res => {
-        let resultObj = {};
-
-        res.forEach((r, i, arr) =>  {
-            if(resultObj[r.sessionId] === undefined) {
-                resultObj[r.sessionId] = [r]
-            } else {
-                resultObj[r.sessionId].push(r)
-            }
-        });
-
-        this.setState({general: Object.keys(resultObj).map(k => resultObj[k])})
-    };
-    // convert results from DB into desired array format for FlatList component
-
-    renderItem = ({item}) => (
-        <View style={generalSummaryStyle.listContainer}>
-            <SelectionRow
-                name= {Moment(item[0].dateEntered).format('LLL')}
-                onPress={() => this.props.navigation.push('generalSession', {resultsArr: item})}
-                icon={Icons.generalDiary + '-outline'}
-            />
-        </View>
+    readDatabaseArg(
+      columns,
+      'DiarySession',
+      this.transformResults,
+      undefined,
+      ' as d inner join Session as s on d.sessionId = s.sessionId inner join Diary as di on d.diaryId = di.diaryId' +
+        " where DATE(diaryDate) = '" +
+        selectedDate +
+        "' and diaryType = 'General'"
     );
+  }
+  // query DB for previous general sessions on this date
 
-    render() {
-        return (
-            <View style={generalSummaryStyle.viewContainer}>
-                <DateChanger
-                    title={Moment(store.getState().diary.date).format('LL')}
-                    forwardFunction={this.forwardDay}
-                    backFunction={this.backDay}
-                />
-                <FlatList
-                    data={this.state.general}
-                    renderItem={this.renderItem}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-            </View>
-        );
-    }
+  transformResults = (res) => {
+    let resultObj = {};
+
+    res.forEach((r, i, arr) => {
+      if (resultObj[r.sessionId] === undefined) {
+        resultObj[r.sessionId] = [r];
+      } else {
+        resultObj[r.sessionId].push(r);
+      }
+    });
+
+    this.setState({ general: Object.keys(resultObj).map((k) => resultObj[k]) });
+  };
+  // convert results from DB into desired array format for FlatList component
+
+  renderItem = ({ item }) => (
+    <View style={generalSummaryStyle.listContainer}>
+      <SelectionRow
+        name={Moment(item[0].dateEntered).format('LLL')}
+        onPress={() => this.props.navigation.push('generalSession', { resultsArr: item })}
+        icon={Icons.generalDiary + '-outline'}
+      />
+    </View>
+  );
+
+  render() {
+    return (
+      <View style={generalSummaryStyle.viewContainer}>
+        <FlatList
+          //data={this.props.coping.sort(compareDates)} // comes from mapStateToProps below
+          data={this.state.general}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
+  }
 }
 
 const generalSummaryStyle = StyleSheet.create({
-    listContainer: {
-        alignSelf: 'stretch'
-    },
+  listContainer: {
+    alignSelf: 'stretch',
+  },
 
-    viewContainer: {
-        flex: 1,
-    },
+  viewContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 
-    buttonText: {
-        fontSize: 18,
-        color: '#007AFF',
-        alignSelf: 'center',
-    },
+  buttonText: {
+    fontSize: 18,
+    color: '#007AFF',
+    alignSelf: 'center',
+  },
 
-    button: {
-        height: 36,
-        backgroundColor: '#fff',
-        borderColor: '#007AFF',
-        borderWidth: 1,
-        borderRadius: 8,
-        margin: 15,
-        alignSelf: 'stretch',
-        justifyContent: 'center',
-    },
+  button: {
+    height: 36,
+    backgroundColor: '#fff',
+    borderColor: '#007AFF',
+    borderWidth: 1,
+    borderRadius: 8,
+    margin: 15,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
 });
