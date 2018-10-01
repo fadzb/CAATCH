a9dfb386e99b74e7fbdc52d659f26579cb182b52
conbag <cogrady91@gmail.com>
2018-10-01 13:25:17 +0100

    implementing day switching within Diary archives

--- Components/DateChanger.js
+++ Components/DateChanger.js
@@ -1,44 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
-import {Icons} from "../Constants/Icon";
-import {PressableIcon} from "./PressableIcon";
-import {AppColors} from "../Styles/TabStyles";
+import PropTypes from 'prop-types';
+import { Icons } from '../Constants/Icon';
 
-export const DateChanger = props => {
-    return (
-        <View style={dateStyle.header}>
-            <PressableIcon
-                iconName={Icons.dividerArrowBack}
-                size={30}
-                onPressFunction={props.backFunction}
-                buttonStyle={{paddingHorizontal: 10}}
-                color={AppColors.blue}
-            />
-            <Text style={{fontSize: 16, color: AppColors.grey}}>{props.title}</Text>
-            <PressableIcon
-                iconName={Icons.dividerArrow}
-                size={30}
-                onPressFunction={props.forwardFunction}
-                buttonStyle={{paddingHorizontal: 10}}
-                color={AppColors.blue}
-            />
-        </View>
-    )
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const DateChanger = (props) => {
+  return (
+    <View style={dateStyle.header}>
+      <Icon name={Icons.dividerArrowBack} size={25} onPress={props.backFunction} />
+      <Text style={{ fontSize: 16 }}>{props.title}</Text>
+      <Icon name={Icons.dividerArrow} size={25} onPress={props.forwardFunction} />
+    </View>
+  );
 };
 
 const dateStyle = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 10,
-        marginHorizontal: 15,
-        marginBottom: 5,
-        backgroundColor: '#f0f0f5',
-        paddingVertical: 8,
-        paddingHorizontal: 15,
-        borderWidth: 1,
-        borderColor: AppColors.orange,
-        borderRadius: 2,
-        alignItems: 'center'
-    }
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    marginHorizontal: 15,
+    marginBottom: 5,
+    backgroundColor: '#f0f0f5',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: 1,
+    borderRadius: 2,
+    alignItems: 'center',
+  },
 });
