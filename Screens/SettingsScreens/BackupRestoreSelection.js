2da09510def66ad3cba419bc5cd575c2397808ee
conbag <cogrady91@gmail.com>
2018-10-19 15:51:32 +0100

    removing all SP media paths in DB when new DB file is restored

--- Screens/SettingsScreens/BackupRestoreSelection.js
+++ Screens/SettingsScreens/BackupRestoreSelection.js
@@ -37,164 +37,150 @@ class BackupRestoreSelection extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            dialogVisible: false,
-            emailInput: ''
+    this.state = {
+      dialogVisible: false,
+      emailInput: '',
+    };
+  }
+
+  showDialog = () => {
+    this.setState({ dialogVisible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ dialogVisible: false });
+  };
+
+  handleSave = () => {
+    this.setState({ dialogVisible: false });
+
+    store.dispatch(updateEmail(this.state.emailInput));
+
+    updateDatabaseArgument(DbTableNames.user, [this.state.emailInput], ['email'], 'where userId = 1');
+  };
+
+  // above for retrieving backup email address from user
+
+  showAlert = () => {
+    Alert.alert(
+      'Enter Backup Email',
+      'Please enter a backup email address first!',
+      [{ text: 'Cancel', onPress: () => console.log('Cancelled'), style: 'cancel' }, { text: 'OK' }],
+      { cancelable: false }
+    );
+  };
+
+  // for when user has no email saved an presses 'backup' button
+
+  showRestoreAlert = (uri) => {
+    Alert.alert(
+      'WARNING',
+      'If you press "Restore" all current data will be overwritten with data from selected file.\n\nPreviously saved media will also be lost.',
+      [
+        { text: 'Cancel', style: 'cancel' },
+        { text: 'Restore', onPress: () => this.restoreDb(uri), style: 'destructive' },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  // main alert for restoring old .db file
+
+  restorePicker = () => {
+    Expo.DocumentPicker.getDocumentAsync({
+      // type: '/*.db',
+      copyToCacheDirectory: false,
+    })
+      .then((res) => {
+        if (res.type !== 'cancel') {
+          if (res.name.includes('caatch.db')) {
+            this.showRestoreAlert(res.uri);
+          } else {
+            Toast.show({
+              text: 'Please select correct .db file',
+              buttonText: 'Okay',
+              duration: 4000,
+              style: { marginBottom: 50 },
+            });
+          }
         }
-    }
+      })
+      .catch((err) => console.log(err));
+  };
+  // opens document picker and ensures that user selects file that contains 'caatch.db' in title
 
-    showDialog = () => {
-        this.setState({ dialogVisible: true });
-    };
+  restoreDb = (dbFile) => {
+    Expo.FileSystem.copyAsync({
+      from: dbFile,
+      to: file,
+    })
+      .then((res) => {
+        const directoryName = 'SafetyplanMedia/';
+        const directory = Expo.FileSystem.documentDirectory + directoryName;
 
-    handleCancel = () => {
-        this.setState({ dialogVisible: false });
-    };
+        Expo.FileSystem.deleteAsync(directory).then((res) => Expo.Updates.reload());
+      })
+      .catch((err) => console.log(err));
+  };
+  // replaces old db file and restarts app
 
-    handleSave = () => {
-        this.setState({ dialogVisible: false });
+  emailBackup = () => {
+    Expo.MailComposer.composeAsync({
+      recipients: [this.props.settings.email],
+      subject: 'CAATCH Backup ' + Moment().format('LL'),
+      body: 'Hi, please find CAATCH backup attached.',
+      attachments: [file],
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+  // opens mail app of choice
 
-        store.dispatch(updateEmail(this.state.emailInput));
-
-        updateDatabaseArgument(DbTableNames.user, [this.state.emailInput], ['email'], 'where userId = 1')
-    };
-
-    // above for retrieving backup email address from user
-
-    showAlert = () => {
-        Alert.alert(
-            'Enter Backup Email',
-            'Please enter a backup email address first!',
-            [
-                {text: 'Cancel', onPress: () => console.log('Cancelled'), style: 'cancel'},
-                {text: 'OK'},
-            ],
-            { cancelable: false }
-        )
-    };
-
-    // for when user has no email saved an presses 'backup' button
-
-    showRestoreAlert = (uri) => {
-        Alert.alert(
-            'WARNING',
-            'If you press "Restore" all current data will be overwritten with data from selected file.\n\nPreviously saved media will also be lost.',
-            [
-                {text: 'Cancel', style: 'cancel'},
-                {text: 'Restore', onPress: () => this.restoreDb(uri), style: 'destructive'},
-            ],
-            { cancelable: false }
-        )
-    };
-
-    // main alert for restoring old .db file
-
-    restorePicker = () => {
-        Expo.DocumentPicker.getDocumentAsync({
-            type: 'text/plain',
-            copyToCacheDirectory: false
-        }).then(res => {
-
-            if(res.type !== 'cancel') {
-                if(Platform.OS === 'ios') {
-                    Expo.FileSystem.downloadAsync(
-                        res.uri,
-                        Expo.FileSystem.cacheDirectory + 'caatch'
-                    ).then(({ uri }) => {
-                        this.showRestoreAlert(uri)
-                    })
-                } else {
-                    this.showRestoreAlert(res.uri)
-                }
-            }
-            // must download file first if user has iOS device
-        }).catch(err => console.log(err))
-    };
-    // opens document picker and ensures that user selects file that contains 'caatch.db' in title
-
-    restoreDb = (dbFile) => {
-        Expo.FileSystem.copyAsync({
-            from: dbFile,
-            to: file
-        })
-            .then(res => {
-                const directoryName = 'SafetyplanMedia/';
-                const directory = Expo.FileSystem.documentDirectory + directoryName;
-
-                Expo.FileSystem.deleteAsync(directory)
-                    .then(res => Expo.Updates.reload())
-            })
-            .catch(err => console.log(err))
-    };
-    // replaces old db file and restarts app
-
-    emailBackup = () => {
-        const txtFile = Expo.FileSystem.cacheDirectory + 'caatch.txt';
-        // converting .db file to .txt so can be read by iOS
-
-        Expo.FileSystem.copyAsync({
-            from: file,
-            to: txtFile
-        })
-            .then(copied => {
-                Expo.MailComposer.composeAsync({
-                    recipients: [this.props.settings.email],
-                    subject: AppName + ' Backup ' + Moment().format('LL'),
-                    body:"Hi, please find " + AppName + " backup attached.\n\nSee restore instructions below:\n\n" + restoreInstructions,
-                    attachments: [txtFile]
-                })
-                    .then(res => console.log(res))
-                    .catch(err => console.log(err))
-            })
-            .catch(err => console.log(err));
-    };
-    // opens mail app of choice
-
-    render() {
-        return (
-            <View style={TabStyles.stackContainer}>
-                <View style={{height: (Dimensions.get('window').height / 11) * 3, alignSelf: 'stretch'}}>
-                    <CustomSelectionRow
-                        name="Backup"
-                        iconContainer={backupRestoreStyle.iconContainer}
-                        //onPress={() => this.props.navigation.push('victory')}
-                        nameStyle={{paddingLeft: 10}}
-                        onPress={this.props.settings.email === 'None' ? this.showAlert : this.emailBackup}
-                    />
-                    <CustomSelectionRow
-                        name="Restore"
-                        iconContainer={backupRestoreStyle.iconContainer}
-                        //onPress={() => this.props.navigation.push('vicSkills')}
-                        nameStyle={{paddingLeft: 10}}
-                        onPress={this.restorePicker}
-                    />
-                    <CustomSelectionRow
-                        name="Set Backup Email"
-                        iconContainer={backupRestoreStyle.iconContainer}
-                        //onPress={() => this.props.navigation.push('insights')}
-                        selectedText={this.props.settings.email}
-                        selectedTextStyle={{color: AppColors.grey}}
-                        selectedTextViewStyle={{flex: .7, alignItems: 'flex-end'}}
-                        nameStyle={{paddingLeft: 10}}
-                        onPress={this.showDialog}
-                    />
-                </View>
-                <Dialog.Container visible={this.state.dialogVisible}>
-                    <Dialog.Title>Backup Email</Dialog.Title>
-                    <Dialog.Description>
-                        Please enter the email address where you would like backup files to be sent
-                    </Dialog.Description>
-                    <Dialog.Input
-                        placeholder={'Email'}
-                        onChangeText={text => this.setState({emailInput: text})}
-                        autoCapitalize={'none'}
-                    />
-                    <Dialog.Button label="Cancel" onPress={this.handleCancel} />
-                    <Dialog.Button label="OK" onPress={this.handleSave} />
-                </Dialog.Container>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={TabStyles.stackContainer}>
+        <View style={{ height: (Dimensions.get('window').height / 11) * 3, alignSelf: 'stretch' }}>
+          <CustomSelectionRow
+            name="Backup"
+            iconContainer={backupRestoreStyle.iconContainer}
+            //onPress={() => this.props.navigation.push('victory')}
+            nameStyle={{ paddingLeft: 10 }}
+            onPress={this.props.settings.email === 'None' ? this.showAlert : this.emailBackup}
+          />
+          <CustomSelectionRow
+            name="Restore"
+            iconContainer={backupRestoreStyle.iconContainer}
+            //onPress={() => this.props.navigation.push('vicSkills')}
+            nameStyle={{ paddingLeft: 10 }}
+            onPress={this.restorePicker}
+          />
+          <CustomSelectionRow
+            name="Set Backup Email"
+            iconContainer={backupRestoreStyle.iconContainer}
+            //onPress={() => this.props.navigation.push('insights')}
+            selectedText={this.props.settings.email}
+            selectedTextStyle={{ color: '#c43a31' }}
+            selectedTextViewStyle={{ flex: 0.7, alignItems: 'flex-end' }}
+            nameStyle={{ paddingLeft: 10 }}
+            onPress={this.showDialog}
+          />
+        </View>
+        <Dialog.Container visible={this.state.dialogVisible}>
+          <Dialog.Title>Backup Email</Dialog.Title>
+          <Dialog.Description>
+            Please enter the email address where you would like backup files to be sent
+          </Dialog.Description>
+          <Dialog.Input
+            placeholder={'Email'}
+            onChangeText={(text) => this.setState({ emailInput: text })}
+            autoCapitalize={'none'}
+          />
+          <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+          <Dialog.Button label="OK" onPress={this.handleSave} />
+        </Dialog.Container>
+      </View>
+    );
+  }
 }
 
 const backupRestoreStyle = StyleSheet.create({
