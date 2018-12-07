a4cca45f1c12b73b09068ccc02083fdbfccb396b
conbag <cogrady91@gmail.com>
2018-12-07 16:21:54 +0000

    updating about screen

--- Screens/SettingsScreens/About.js
+++ Screens/SettingsScreens/About.js
@@ -9,6 +9,7 @@ import {
   Dimensions,
   TouchableOpacity,
   Platform,
+  Image,
 } from 'react-native';
 import { Icons } from '../../Constants/Icon';
 import { AppColors, TabStyles } from '../../Styles/TabStyles';
@@ -24,10 +25,53 @@ export default class About extends React.Component {
   render() {
     return (
       <View style={TabStyles.stackContainer}>
-        <Text>test</Text>
+        <View style={{ overflow: 'hidden', borderRadius: 10, marginVertical: 20 }}>
+          <Image source={require('../../Media/Images/SafePlanLogo.png')} style={{ width: 80, height: 80 }} />
+        </View>
+        <View style={aboutStyle.textContainer}>
+          <Text style={{ paddingVertical: 20, fontSize: 16, color: AppColors.blue }}>
+            SafePlan was developed in conjunction with the HSE's "Connecting for Life" programme.
+          </Text>
+          <View style={aboutStyle.paraContainer}>
+            <Text style={aboutStyle.headText}>Developer</Text>
+            <Text style={aboutStyle.paraText}>Conor O'Grady</Text>
+          </View>
+          <View style={aboutStyle.paraContainer}>
+            <Text style={aboutStyle.headText}>Design Team</Text>
+            <Text style={aboutStyle.paraText}>
+              Dr. Jim Duggan, Karen Young, Dr. Ruth Melia, Dr. John Bogue, Mary O'Sullivan
+            </Text>
+          </View>
+          <View style={aboutStyle.paraContainer}>
+            <Text style={aboutStyle.paraText}>
+              {'NUI Galway School of Computer Science\nNUI Galway School of Psychology'}
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
-const aboutStyle = StyleSheet.create({});
+const aboutStyle = StyleSheet.create({
+  textContainer: {
+    alignSelf: 'flex-start',
+    marginHorizontal: 20,
+  },
+
+  paraContainer: {
+    marginVertical: 15,
+  },
+
+  headText: {
+    color: AppColors.grey,
+    fontWeight: Platform.OS === 'ios' ? '600' : '500',
+    fontSize: 16,
+    paddingBottom: 5,
+  },
+
+  paraText: {
+    color: AppColors.blue,
+    fontSize: 16,
+  },
+});
